579c6590c411e06b509bf2e22298bffcc28f113e
doabit <doinsist@gmail.com>
2014-01-16 11:18:14 +0800

    Set set_page_title, set_page_description, set_page_keywords can work in views

--- lib/seo_helper/railtie.rb
+++ lib/seo_helper/railtie.rb
@@ -4,6 +4,7 @@ module SeoHelper
   class Railtie < Rails::Railtie
     initializer "seo_helper.view_helpers" do
       ActionView::Base.send :include, SeoHelper::Helper
+      ActionView::Base.send :include, SeoHelper::ControllerHelper
     end
 
     initializer "seo_helper.controller_helpers" do
